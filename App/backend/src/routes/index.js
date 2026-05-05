15dca88025c5c14c06a919bf6831c55cc348cc1a
Sato <s.lutereau@hotmail.com>
2026-05-05 13:44:05 +0100

    carreras rutas y validaciones

--- App/backend/src/routes/index.js
+++ App/backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes.js';
+import carreraRoutes from './carrera.routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/carreras', carreraRoutes);
 
 export default router;
