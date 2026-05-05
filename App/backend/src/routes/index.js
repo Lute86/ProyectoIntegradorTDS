14617b27eddd59f4ca2104aa16f49320fa600c7d
Sato <s.lutereau@hotmail.com>
2026-05-05 14:00:46 +0100

    crud completo materias

--- App/backend/src/routes/index.js
+++ App/backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes.js';
 import carreraRoutes from './carrera.routes.js';
+import materiaRoutes from './materia.routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/carreras', carreraRoutes);
+router.use('/materias', materiaRoutes);
 
 export default router;
